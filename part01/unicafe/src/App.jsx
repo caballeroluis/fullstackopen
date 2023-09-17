e4f0443648ed2c94ec181c4c11414e9fa55b42d5
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2023-09-17 21:11:45 +0200

    1.7: unicafe step2

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -2,42 +2,63 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to its own state
+    // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [comments, setComments] = useState([])
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setComments([...comments, 1])
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setComments([...comments, 0])
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setComments([...comments, -1])
+  }
+
+  const averageScore = () => {
+    if (comments.length === 0) {
+      return 0
+    }
+    const totalScore = comments.reduce((acc, score) => acc + score, 0)
+    return totalScore / comments.length
+  }
+
+  const positivePercentage = () => {
+    if (comments.length === 0) {
+      return 0
+    }
+    const positiveCount = comments.filter((score) => score === 1).length
+    return (positiveCount / comments.length) * 100
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      
+
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
 
-      <h1>statics</h1>
-
+      <h1>statistics</h1>
+      
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {comments.length}</p>
+      <p>average {averageScore()}</p>
+      <p>positive {positivePercentage()}%</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 export default App
