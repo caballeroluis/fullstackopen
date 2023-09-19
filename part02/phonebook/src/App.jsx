eaffb5fad49ff325ae01bea595467bcc0f0925de
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2023-09-19 21:26:05 +0200

    2.7: The Phonebook Step2

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -11,9 +11,16 @@ const App = () => {
   }
 
   const addPerson = (event) => {
-    event.preventDefault()
-    const newPerson = { name: newName };
-    setPersons([...persons, newPerson]);
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons([...persons, newPerson]);
+
+      setNewName('');
+    }
   }
 
   return (
