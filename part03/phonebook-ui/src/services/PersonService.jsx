4c0c42ed0764cf1d0ce880524f8698219830e188
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2023-09-24 12:45:49 +0200

    3.9 phonebook backend step9

--- part03/phonebook-ui/src/services/PersonService.jsx
+++ part03/phonebook-ui/src/services/PersonService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001'
+const baseUrl = 'http://localhost:3001/api'
 
 const getAll = () => {
   const request = axios.get(`${baseUrl}/persons`)
