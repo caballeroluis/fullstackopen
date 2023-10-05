0e0022691c323d22a89b7c8246d403710a9e5c65
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2023-10-05 15:50:03 +0200

    4.9: Blog list tests, step2

--- part04/tests/blog_api.test.js
+++ part04/tests/blog_api.test.js
@@ -20,6 +20,15 @@ test('blogs are returned as json and the correct number of blogs', async () => {
   expect(response.body).toHaveLength(listHelper.blogs.length)
 })
 
+test('blogs have "id" property instead of "_id"', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach((blog) => {
+    expect(blog.id).toBeDefined()
+    expect(blog._id).toBeUndefined()
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
